cd730e109cc64dd3a227b877c615f18ba735b04f
João Paulo <joaopwii@gmail.com>
2025-08-14 11:39:10 -0300

    comecando

---
+++ Conselho/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+Console.WriteLine("Digite algo para receber um conselho!");
+Console.ReadLine();
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://api.adviceslip.com/advice");
+        var advice = response;
+        Console.WriteLine(advice);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"We have a problem ({ex.Message})");
+    }
+}
