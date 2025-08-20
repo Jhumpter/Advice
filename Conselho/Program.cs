92edfdb53f4ee38b2a051910453529298e0fff6c
João Paulo <joaopwii@gmail.com>
2025-08-20 11:34:31 -0300

    Update Program.cs

--- Conselho/Program.cs
+++ Conselho/Program.cs
@@ -1,20 +1,59 @@
 ﻿using Conselho;
+using System.ComponentModel.Design;
 using System.Text;
 using System.Text.Json;
 
-Console.WriteLine("Type something to get advice");
-Console.ReadLine();
+Console.WriteLine("Hello!");
+Thread.Sleep(1000);
+await GetAdvice();
+await Menu();
 
-using (HttpClient client = new HttpClient())
+async Task Menu()
 {
+    Console.WriteLine("1 - New advice");
+    Console.WriteLine("2 - Exit");
+    Console.Write("Choose an option: ");
+    int option = 0;
     try
     {
-        string response = await client.GetStringAsync("https://api.adviceslip.com/advice");
-        var advice = JsonSerializer.Deserialize<Resposta>(response);
-        Console.WriteLine(advice.slip.advice);
+        option = int.Parse(Console.ReadLine());
     }
-    catch (Exception ex)
+    catch (FormatException)
     {
-        Console.WriteLine($"We have a problem ({ex.Message})");
+        Console.WriteLine("Invalid input, please enter a number.");
+        await Menu();
+    }
+    if (option == 1)
+    {
+        await GetAdvice();
+        await Menu();
+    }
+    else if (option == 2)
+    {
+        Console.WriteLine("Goodbye!");
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("Invalid option, try again.");
+        await Menu();
     }
 }
+
+async Task GetAdvice()
+{
+    using (HttpClient client = new HttpClient())
+    {
+        try
+        {
+            string response = await client.GetStringAsync("https://api.adviceslip.com/advice");
+            var advice = JsonSerializer.Deserialize<Resposta>(response);
+            Console.WriteLine(advice.slip.advice);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"We have a problem ({ex.Message})");
+        }
+    }
+}
+
